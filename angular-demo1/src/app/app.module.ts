ab91d3752b9d461fa4a95226c745b48e8cef97ed
Albert Li <ali429@uwo.ca>
2018-11-17 23:39:51 +0000

    third commit

--- angular-demo1/src/app/app.module.ts
+++ angular-demo1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
+import { AutoGrowDirective } from './auto-grow.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    AutoGrowDirective
   ],
   imports: [
     BrowserModule,
